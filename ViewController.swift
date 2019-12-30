cbaf88a247a061683733cbd4c2df7c7f0273463f
vigneshiosdev <53249276+vigneshiosdev@users.noreply.github.com>
2019-12-30 16:11:17 +0530

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -98,3 +98,131 @@ class ViewController: UIViewController {
 }
 
 
+//upload
+/*
+func APIStep1() {
+    
+    let myUrl = NSURL(string: "\(demourl)educator_register_s1.php")
+    
+    let request = NSMutableURLRequest(url:myUrl! as URL)
+    request.httpMethod = "POST"
+    
+    let param = [
+        "name"  : self.txtName.text!,
+        "gender" : self.strGender,
+        "marital_status" : self.txtMartialStatus.text!,
+        "dob" : self.txtDOB.text!,
+        "race" : self.strRace,
+        "citizenship" : self.strCitizenship,
+        "address" : self.txtAddress.text!,
+        "postal_code" : self.txtPostalCode.text!,
+        "mobile" : self.txtMobile.text!,
+        "high_edu_level" : self.txtEducationalLevel.text!,
+        "email" : self.txtEmail.text!,
+        "password" : self.txtPassword.text!,
+        "experience" : self.txtExperience.text!,
+        "description" : self.txtViewDescription.text!
+    ]
+    let boundary = generateBoundaryString()
+    
+    request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+    
+    
+    let imageData = self.selImage!.jpegData(compressionQuality: 0.1)
+    
+    if(imageData==nil)  { return; }
+    
+    request.httpBody = createBodyWithParameters(parameters: param, filePathKey: "profile_img", imageDataKey: imageData! as NSData, boundary: boundary) as Data
+    
+    
+    
+    let task = URLSession.shared.dataTask(with: request as URLRequest) {
+        data, response, error in
+        
+        if error != nil {
+            return
+        }
+        
+        // You can print out response object
+        
+        // Print out reponse body
+        let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+        print("****** response data = \(responseString!)")
+        
+        do {
+            let json = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary
+            
+            print(json!)
+            
+            DispatchQueue.main.async {
+                let statusT = json!["status"] as! String
+                let messageT = json!["message"] as! String
+                self.view.makeToast(messageT)
+                if(statusT == "1"){
+                    let id = json!["id"] as! Int
+                    
+                    UserDefaults.standard.set(id, forKey: "TempLoginID")
+                    UserDefaults.standard.synchronize()
+                    
+                    self.appdelegate.stepCompleted = 1
+                    let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                    let next = storyBoard.instantiateViewController(withIdentifier: "Register2VC") as! Register2VC
+                    self.present(next , animated: false, completion: nil)
+                    
+                }else{
+                    self.view.makeToast(messageT)
+                }
+            }
+        }catch
+        {
+            print(error)
+        }
+        
+    }
+    
+    task.resume()
+}
+
+func createBodyWithParameters(parameters: [String: String]?, filePathKey: String?, imageDataKey: NSData, boundary: String) -> NSData {
+    let body = NSMutableData();
+    
+    if parameters != nil {
+        for (key, value) in parameters! {
+            body.appendString(string: "--\(boundary)\r\n")
+            body.appendString(string: "Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n")
+            body.appendString(string: "\(value)\r\n")
+        }
+    }
+    let mimetype = "image/jpeg"
+    body.appendString(string: "--\(boundary)\r\n")
+    body.appendString(string: "Content-Disposition: form-data; name=\"\(filePathKey!)\"; filename=\"file.jpeg\"\r\n")
+    body.appendString(string: "Content-Type: \(mimetype)\r\n\r\n")
+    body.append(imageDataKey as Data)
+    body.appendString(string: "\r\n")
+    body.appendString(string: "--\(boundary)--\r\n")
+    return body
+}
+
+func generateBoundaryString() -> String {
+    return "Boundary-\(NSUUID().uuidString)"
+}
+
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+}
+extension NSMutableData {
+    func appendString(string: String) {
+        let data = string.data(using: String.Encoding.utf8, allowLossyConversion: true)
+        append(data!)
+    }
+}
+
+*/
